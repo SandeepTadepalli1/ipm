a75f0b3d042c1cd4e04a03215867e803a031b944
sandeeptadepalli1 <tadepallisandeep1@gmail.com>
2016-11-22 02:20:52 +0530

    Added Roles for Director IpCommittee

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,48 @@
 class UsersController < ApplicationController
-	before_action :authenticate_user!, only: [:show]
+	before_action :authenticate_user!, only: [:show, :addipcommittee]
+	before_action :isdirector, only: [:addipcommitee]
 	# before_action :set_user
-	def show
+	def dashboard
 		@user = User.find_by username: params[:username]
 	end
+
+	def addipcommittee
+		if(current_user.role != 'director')
+			redirect_to user_path(current_user.username), notice: "Not Authorized to Edit these Info"
+		end
+		@nonips = (User.where.not(role: ['director','ipcommittee']))
+	end
+
+	def removeipcommittee
+		if(current_user.role != 'director')
+			redirect_to user_path(current_user.username), notice: "Not Authorized to Edit these Info"
+		end
+		@ips =User.where(role:'ipcommittee')
+	end
+
+	def changeroletoIpcommittee
+		params[:selected_users].each do |uid|
+			a=User.find(uid)
+			a.role='ipcommittee'
+			a.save
+		end
+		redirect_to user_path(current_user.username) 
+	end
+
+	def changeroletousers
+		params[:selected_users].each do |uid|
+			a=User.find(uid)
+			a.role='user'
+			a.save
+		end
+		redirect_to user_path(current_user.username) 
+	end
+
+	private
+		def isdirector
+			if(current_user.role != 'director')
+				puts current_user.role
+				redirect_to root_path, notice: "Not Authorized to Edit these Info"
+			end
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,16 @@ Rails.application.routes.draw do
   devise_for :users
   root 'ip_assets#index'
   # get 'dashboard' => 'users#show' ,as: 'user'
-  get ':username' => 'users#show' , as: 'user' # As user gives us access to the user_path
+  get ':username' => 'users#dashboard' , as: 'user' # As user gives us access to the user_path
+  
+  get 'users/addipcommittee'
+  get 'users/removeipcommittee'
+  
+  post 'users/changeroletoIpcommittee'
+  post 'users/changeroletousers'
+  
+  # member do
+  # 	post '/changeroletoIpcommittee' => 'users#changeroletoIpcommittee'
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
